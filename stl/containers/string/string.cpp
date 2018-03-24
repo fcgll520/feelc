3f349da6fefa8999115524f02c5595449d0eae21
feelc <634609116@qq.com>
2018-03-24 08:24:17 +0800

    add

--- stl/containers/string/string.cpp
+++ stl/containers/string/string.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <stdio.h>
+#include <stdint.h>
 
 using std::string;
 
@@ -35,17 +36,79 @@ int main(int argc, char const *argv[])
 	}
 	printf("\n\n");
 
+	printf("func operator[]\n");
+	for (uint32_t i = 0; i < str.length(); ++i)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n\n");
+
+	printf("func at\n");
+	for (uint32_t i = 0; i < str.length(); ++i)
+	{
+		printf("%c", str.at(i));
+	}
+	printf("\n\n");
+
+
 	printf("func size, length, capacity, max_size\n");
 	printf("size: %u, length: %u, capacity: %u, max_size: %u\n", str.size(), str.length(),
 		str.capacity(), str.max_size());
 	printf("\n");
 
+	//扩充或者截取字符串
 	printf("func resize 10\n");
 	str.resize(10, 'x');
 	printf("str: %s, size: %u, capacity: %u\n", str.c_str(), str.size(), str.capacity());
 	printf("func resize 3\n");
-	str.resize(3);
-	printf("str: %s, size: %u, capacity: %u\n", str.c_str(), str.size(), str.capacity());
+	str.resize(6);
+	printf("str: %s, size: %u, capacity: %u\n\n", str.c_str(), str.size(), str.capacity());
+
+	//增大 capacity
+	printf("func reserve 100\n");
+	str.reserve(100);
+	printf("str: %s, size: %u, capacity: %u\n\n", str.c_str(), str.size(), str.capacity());
+
+	//将 capacity 减少到和length一样大
+	printf("func shrink_to_fit\n");
+	str.shrink_to_fit();
+	printf("str: %s, size: %u, %u, capacity: %u\n\n", str.c_str(), str.size(), str.capacity());
+
+	printf("func back, front\n");
+	str.back() = 'F';
+	printf("str: %s\n", str.c_str());
+	str.front() = 'A';
+	printf("str: %s\n\n", str.c_str());
+
+	str = "abcdef";
+	printf("func c_str, data\n");
+	printf("str: %s, %s\n\n", str.c_str(), str.data());
+
+	printf("func get_allocator\n");
+	char *pos = str.get_allocator().allocate(5);
+	for (int i = 0; i < 5; ++i)
+	{
+		str.get_allocator().construct(&pos[i], i+100);
+	}
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("%d,", pos[i]);
+	}
+	printf("\n");
+	printf("str: %s\n\n", str.c_str());
+	for (int i = 0; i < 5; ++i)
+	{
+		str.get_allocator().destroy(&pos[i]);
+	}
+	str.get_allocator().deallocate(pos, 5);
+
+	printf("func copy\n");
+	char buffer[20] = { '\0' };
+	// 跳过3个字符，从第四个字符开始拷贝10个字节，返回拷贝成功的字节数
+	size_t copy_len = str.copy(buffer, 10, 3);
+	printf("buffer: %s, copy_len: %u\n\n", buffer, copy_len);
+
+
 	
 
 	return 0;
